669609ea10fdfade8db92f1fa26cf64ce5180ee8
Nicolas Campos <nicolascampos611@gmail.com>
2025-04-11 21:19:42 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+const form = document.getElementById("entryForm");
+const titleInput = document.getElementById("title");
+const typeSelect = document.getElementById("type");
+const contextInput = document.getElementById("context");
+const entrieslist = document.getElementById("entriesList");
+
+let editingId = null;
+
+function getEntries() {
+    return JSON.parse(localStorage.getItem("english_diary_entries") || "[]");
+} //Le os dados do navegador. Se não houver nada, retorna uma lista vazia
+
+function saveEntries(entries) {
+    localStorage.setItem("english_diary_entries", JSON.stringify(entries));
+} //Pega a lista de aprendizados e salva no navegador 
+
+function addEntry(entry) {
+    const entries = getEntries();
+    entries.unshift(entry);
+    saveEntries(entries);
+} //Cria uma nova entrada e salva com as anteriores
+
+function editEntry(id) {
+    const entry = getEntries().find(e => e.id === id);
+    if (entry) {
+        titleInput.value = entry.title;
+        typeSelect.value = entry.type;
+        contextInput.value = entry.context;
+        editingId = id;
+    }
+} //Preenche o formulário com os dados do item escolhido para editar
+
+function updateEntry(updateEntry) {
+    const entries = getEntries().map(entry =>
+        entry.id === updateEntry.id ? updateEntry : entry
+    );
+    saveEntries(entries);
+} //Substitui o item antigo pelo novo(baseado no id)
+
+function deleteEntry(id) {
+    const entries = getEntries().filter(entry => entry.id !== id);
+    saveEntries(entries);
+    loadEntries(); //Atualiza a tela
+} //Remove o item da lista
+
+function loadEntries() {
+    const entries = getEntries();
+    entrieslist.innerHTML = "";
+
+    entries.forEach(entry => {
+        const div = document.createElement("div");
+        div.innerHTML = `
+            <strong>${entry.title}</strong> [${entry.type}]<br>
+            <em>${entry.context}</em><br>
+            <small>${new Date(entry.date).toLocaleString()}</small></br>
+            <button onclick="editEntry('${entry.id}')">Editar</button>
+            <button onclick="deleteEntry('${entry.id}')">Excluir</button>
+            <hr>`;
+        entrieslist.appendChild(div);
+    })
+} //Cria blocos HTML com os dados salvos e mostra na tela
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); //Evita o recarregamento da página
+
+    const entry = {
+        id: editingId || Date.now().toString(),
+        title: titleInput.value.trim(),
+        type: typeSelect.value,
+        context: contextInput.value.trim(),
+        date: new Date().toISOString
+    };
+
+    if (!entry.title) return; //Se o título estiver vazio, cancela
+
+    if (editingId) {
+        updateEntry(entry); //Atualiza
+        editingId = null;
+    } else {
+        addEntry(entry); //Adiciona novo
+    }
+
+    form.reset(); //Limpa o formulário
+    loadEntries(); //Atualiza a lista
+}); //Cria ou atualiza um item ao clicar em salvar
+
+loadEntries(); //Ao abrir a página, ele mosrta os intems salvos
